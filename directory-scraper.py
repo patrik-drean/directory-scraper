7418a4f86b435b99c01ce6bac7064f1e667ebcd7
Patrik <patrikdrean@gmail.com>
2019-10-14 08:32:13 -0600

    output json

--- directory-scraper.py
+++ directory-scraper.py
@@ -9,21 +9,23 @@ from selenium.common.exceptions import NoSuchElementException, StaleElementRefer
 login_url = "https://ident.churchofjesuschrist.org/sso/UI/Login"
 directory_url = "https://directory.churchofjesuschrist.org/2029413"
 household_class_name = "sc-gZMcBi"
+creds_file_path = "../secrets/lds-creds.json"
 
-json_file = open('../secrets/lds-creds.json')
-data = json.load(json_file)
+json_file = open(creds_file_path)
+creds = json.load(json_file)
 
 def main():
   driver = configureDriver()
 
   authenticateUser(driver)
   households = getHouseholds(driver)
+  outputJson(households)
 
 def configureDriver():
   options = webdriver.ChromeOptions()
   options.add_argument('--ignore-certificate-errors')
   options.add_argument('--incognito')
-  options.add_argument('--headless')
+  # options.add_argument('--headless')
   driver = webdriver.Chrome("/Users/patrikdrean/dev/directory-scraper/chromedriver", options=options)
   return driver
 
@@ -34,26 +36,41 @@ def authenticateUser(driver):
   # username = input('Enter username: ')
   # password = getpass.getpass('Enter password: ')
 
-  username = data['username']
-  password = data['password']
+  username = creds['username']
+  password = creds['password']
 
   WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'IDToken1')))  
   username_input = driver.find_element_by_name("IDToken1")
   password_input = driver.find_element_by_name("IDToken2")
 
   username_input.clear()
-  username_input.send_keys(username)
   password_input.clear()
+  username_input.send_keys(username)
   password_input.send_keys(password)
 
   username_input.submit()
 
 def getHouseholds(driver):
   driver.get(directory_url)
-  WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, household_class_name)))
+  WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, household_class_name)))
   driver.find_elements_by_class_name(household_class_name)
   
   households = list(map(lambda element: element.text, driver.find_elements_by_class_name(household_class_name)))
+  return households
+
+def outputJson(households):
+  json_string = '['
+
+  for row in households: 
+    json_string += f'{{ "name": "{row}"}},'
+    
+  json_string = json_string[:-1]
+  json_string += ']'
+
+  f = open("households.json", "w")
+  f.write(json_string)
+  f.close()
+
 
 if __name__ == "__main__":
     main()
