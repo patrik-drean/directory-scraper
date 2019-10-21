2151399f220cc28b36877d78f5e4e47b88806068
Patrik <patrikdrean@gmail.com>
2019-10-20 20:56:47 -0600

    spreadsheet is updated for both households and archive tabs

--- google-api.py
+++ google-api.py
@@ -4,42 +4,57 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SPREADSHEET_ID = '1Ly_6nh1QfqQufMwwT69n1GUtZ72hFaynO6visbJFpRA'
-RANGE = 'Households!A1:P800'
+RANGE = 'A1:P800'
 CREDS_FILE_PATH = "../secrets/lds-creds.json"
 
-def main():
-    spreadsheet_service = authenticate()
-    household_rows = getSheetValues(spreadsheet_service)
+def getSheetData(sheet_name):
+  spreadsheet_service = authenticate()
 
-    for row in household_rows:
-        print(row)
-
-def authenticate():
-    creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(CREDS_FILE_PATH, SCOPES)
-            creds = flow.run_local_server(port=0)
-
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    return build('sheets', 'v4', credentials=creds)
-
-def getSheetValues(spreadsheet_service):
   sheet = spreadsheet_service.spreadsheets()
-  result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,range=RANGE).execute()
+  result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,range=f'{sheet_name}!{RANGE}').execute()
   values = result.get('values', [])
 
   return values
 
-if __name__ == "__main__":
-    main()
+def writeDataToSheet(sheet_name, values):
+  spreadsheet_service = authenticate()
+
+  for _ in range(0, 100):
+    values.append(['', '', ''])
+
+  body = {
+      'values': values
+  }
+  result = spreadsheet_service.spreadsheets().values().update(
+      spreadsheetId=SPREADSHEET_ID, range=f'{sheet_name}!{RANGE}',
+      valueInputOption='RAW', body=body).execute()
+
+def appendDataToSheet(sheet_name, values):
+  spreadsheet_service = authenticate()
+
+  body = {
+      'values': values
+  }
+  result = spreadsheet_service.spreadsheets().values().append(
+      spreadsheetId=SPREADSHEET_ID, range=f'{sheet_name}!{RANGE}',
+      valueInputOption='RAW', body=body).execute()
+
+def authenticate():
+  creds = None
+  if os.path.exists('token.pickle'):
+      with open('token.pickle', 'rb') as token:
+          creds = pickle.load(token)
+
+  if not creds or not creds.valid:
+      if creds and creds.expired and creds.refresh_token:
+          creds.refresh(Request())
+      else:
+          flow = InstalledAppFlow.from_client_secrets_file(CREDS_FILE_PATH, SCOPES)
+          creds = flow.run_local_server(port=0)
+
+      with open('token.pickle', 'wb') as token:
+          pickle.dump(creds, token)
+
+  return build('sheets', 'v4', credentials=creds)
--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 directory_scraper = __import__('directory-scraper')
+google_api = __import__('google-api')
 
+ARCHIVE_RECORDS = []
+UPDATED_GOOGLE_RECORDS = []
 
 def main():
-  print(directory_scraper.getCurrentHouseholds())
-  
+  current_households = directory_scraper.getCurrentHouseholds()
+  google_records = google_api.getSheetData('Households')
+
+  headers = [google_records[0]]
+
+  for record in google_records[1:]:
+    household_name = record[0]
+
+    if household_name in current_households:
+      UPDATED_GOOGLE_RECORDS.append(record)
+      current_households.remove(household_name)
+    else:
+      ARCHIVE_RECORDS.append(record)
+
+  for new_household in current_households:
+    UPDATED_GOOGLE_RECORDS.append([new_household, '-','-'])
+
+  google_output = headers + sorted(UPDATED_GOOGLE_RECORDS, key=lambda x: x[0])
+
+  google_api.writeDataToSheet('Households', google_output)
+  google_api.appendDataToSheet('Archive',ARCHIVE_RECORDS)
 
 if __name__ == "__main__":
     main()
